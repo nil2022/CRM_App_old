af47a2e2cc12b90c56e065a51c13568ebcd11355
Nilanjan Haldar <nil.haldar@gmail.com>
2023-02-12 01:08:16 +0530

    changes

--- configs/db.config.js
+++ configs/db.config.js
@@ -1,4 +1,4 @@
 module.exports = {
     DB_NAME: "crm_db",
-    DB_URL: "mongodb://localhost:27017/crm_db"
+    DB_URL: "mongodb+srv://myDB:2022%40Kanu@cluster0.l4j2gkp.mongodb.net/?retryWrites=true&w=majority"
 }
